eebcdc6802a4e7fc24c5e5822c3cc716a14ea4a0
Gilang Pratama <pratamapriadi96@gmail.com>
2022-02-22 01:19:13 +0700

    :zap: Update inmates_face_training_model.py

--- models/inmates/inmates_face_training_model.py
+++ models/inmates/inmates_face_training_model.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 
 class FaceTrainingOut(BaseModel):
     acc: float
-    val_acc: float
+    # val_acc: float
     loss: float
-    val_loss: float
+    # val_loss: float
