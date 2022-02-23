a321e55685a66119a0b62f279012e5023275b969
Gilang Pratama <pratamapriadi96@gmail.com>
2022-02-23 15:13:47 +0700

    :zap: Update report_route.py

--- routers/report_route.py
+++ routers/report_route.py
@@ -15,9 +15,9 @@ async def show_report():
     return paginate(responses)
 
 
-@router.get("/{month}/{year}", response_model=Page[InmatesScoreOut])
+@router.get("/{month}/{year}", response_model=Page[ReportOut])
 async def show_report_monthly(month: int, year: int):
-    responses = await fetch_all_inmates_score()
+    responses = await fetch_atendance()
     this_date = datetime.datetime.strptime(f"{year}-{month}", "%Y-%m")
     responses = [response for response in responses if response.created_at >= this_date]
     return paginate(responses)
